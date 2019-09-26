3efba130869dc02850f034801948b1b452e426af
fahad <fahdjamy2@gmail.com>
2019-09-26 11:54:22 +0300

    ch(add swagger): Add swagger documentations.
    - add swagger to documentation the API.
    
    [Finishes]

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,14 @@
+import YAML from "yamljs";
+import path from "path";
+import swaggerUI from "swagger-ui-express";
 import userRoute from "./app/user.route";
 import imageRoute from "./app/image.route";
 
+const swaggerDoc = YAML.load(path.join(__dirname, "../../documentation.yml"));
+
 const apiV = "/api/v1";
 const routes = app => {
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
   app.use(`${apiV}/auth`, userRoute);
   app.use(`${apiV}/image`, imageRoute);
 };
